3c3e4db2008492c02344bd1f3c6a11f9d6501d27
Dev Gangwar <gangwardev50@gmail.com>
2022-09-07 11:30:25 +0530

    Adding Remove Functionality

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,35 +1,42 @@
 import { useEffect, useState } from 'react'
 
 export default function Todo() {
-  const[text, setTxt] = useState(" Hello" );
+  const [text, setTxt] = useState(" Hello");
   const [task, setTask] = useState([]);
-  
 
-  const handleOnClick = (e)=>{
+  // decalring function for add button
+  const handleOnClick = (e) => {
     e.preventDefault();
-    setTask([...task, text])
-    setTxt(" ");
-   
-    
+    const allInputData = {id: Math.floor(Math.random()*100), name: text };
+    setTask([...task, allInputData])
+    console.log(allInputData)
+    setTxt("");
   }
- 
- 
- 
+  // declaring function for remove button
+  const remove=(index)=>{
+  const finalData =   task.filter((item)=>{
+    return index !== item.id
+  })
+  setTask(finalData)
+  }
+
+
+
 
   return (
 
     <div className="input">
       <header>
-        <input type="text" value={text}  onChange={(e)=>setTxt(e.target.value)}></input>
+        <input type="text" value={text} onChange={(e) => setTxt(e.target.value)}></input>
+        <button type='Submit' onClick={handleOnClick}>ADD</button>
         {/* <button className="btn btn-danger" onClick={remove}>Remove2</button> */}
-        {task.map((item)=>{
-         return <><div className="para"><p>{item}</p></div></>
+        {task.map((item) => {
+          return <><div className="para"><p>{item.name}</p> <button onClick={()=>remove(item.id)}>Remove</button> </div></>
 
         })}
-       
-           
-      
-        <button type='Submit' onClick={handleOnClick}>ADD</button>
+
+
+
       </header>
     </div>
   )
